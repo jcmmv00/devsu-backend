bc0b0bdf5466e3126cc2eb6d6f8c3de748017cf7
Joseph M <jcmmv00@gmail.com>
2024-09-18 22:33:21 -0500

    PDF

--- src/main/java/com/devsu/devsu/application/controllers/MovementController.java
+++ src/main/java/com/devsu/devsu/application/controllers/MovementController.java
@@ -1,5 +1,6 @@
 package com.devsu.devsu.application.controllers;
 
+import java.io.ByteArrayOutputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -10,6 +11,9 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +29,11 @@ import com.devsu.devsu.core.exceptions.DailyLimitExceeded;
 import com.devsu.devsu.core.exceptions.MovementBalanceNotAvailableException;
 import com.devsu.devsu.core.model.Movement;
 import com.devsu.devsu.core.services.MovementService;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
 
 import jakarta.validation.Valid;
 
@@ -66,7 +75,54 @@ public class MovementController {
     public ResponseEntity<List<MovementViewQuery>> getAllMovements() throws AccountNotFoundException {
         List<MovementViewQuery> movements = movementService.getAllMovements().stream()
                 .map(m -> new MovementViewQuery(m.getMovementId(), m.getDate(), m.getQuantity(), m.getMovementType(),
-                        m.getAccount().getAccountNumber())).collect(Collectors.toList());
+                        m.getAccount().getAccountNumber()))
+                .collect(Collectors.toList());
         return ResponseEntity.ok(movements);
     }
+
+    @PostMapping(value = "/reportPDF")
+    public ResponseEntity<byte[]> generatePdf(@RequestParam("date") String dateRange,
+            @RequestParam("accountNumber") String accountNumber) throws AccountNotFoundException, ParseException {
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+            String[] dates = dateRange.split(",");
+            Date dateA = new SimpleDateFormat("dd-MM-yyyy").parse(dates[0]);
+            Date dateB = new SimpleDateFormat("dd-MM-yyyy").parse(dates[1]);
+
+            PdfDocument pdfDoc = new PdfDocument(new PdfWriter(out));
+            Document document = new Document(pdfDoc);
+
+            document.add(new Paragraph("Movements - accountNumber: " + accountNumber));
+            document.add(new Paragraph("From: " + sdf.format(dateA) + " To: " + sdf.format(dateB)));
+
+            Table table = new Table(4);
+            table.addCell("Account Number");
+            table.addCell("Quantity");
+            table.addCell("Movement Type");
+            table.addCell("Date");
+
+            List<Movement> movements = movementService.queryMovementsBydateRange(dateA, dateB, accountNumber);
+
+            for (Movement movement : movements) {
+                table.addCell(movement.getAccount().getAccountNumber());
+                table.addCell(movement.getQuantity().toString());
+                table.addCell(movement.getMovementType().name());
+                table.addCell(sdf.format(movement.getDate()));
+            }
+
+            document.add(table);
+            document.close();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_PDF);
+            headers.setContentDispositionFormData("filename", "reporte.pdf");
+
+            return new ResponseEntity<>(out.toByteArray(), headers, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
